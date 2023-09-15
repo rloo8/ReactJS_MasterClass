6bba20324d3ebfe903dfdd8f1895286a0262cfa7
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-15 17:09:54 +0900

    Event

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
 function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <Container>
-        <H1>HELLO</H1>
-      </Container>
+      <button onClick={onClick}>CLick</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
