b08b7db3c1eb2c14a2eac13a7c31f46f6e727b8d
rloo8 <141135378+rloo8@users.noreply.github.com>
2023-09-15 16:50:53 +0900

    Theme/typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={value} type="text" />
-        <button>Submit</button>
-      </form>
+      <Container>
+        <H1>HELLO</H1>
+      </Container>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-root.render(<App />);
